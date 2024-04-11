72db65af788444e48138106b19608c2d127d3576
hannahmeg <hannah_arcega@yahoo.com>
2024-04-11 10:56:53 +0800

    Seeded new data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# 100.times do |index|
-#   Post.create (title: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 0).chop,
-#     content: Faker::Lorem.paragraph)
-# end
+100.times do |index|
+  Post.create(title: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 0).chop,
+    content: Faker::Lorem.paragraph)
+end
 
 20.times do |index|
   student = Student.create(
