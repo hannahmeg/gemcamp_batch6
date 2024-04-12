59919081d011df0feae446ac0e3164ae551ba0de
hannahmeg <hannah_arcega@yahoo.com>
2024-04-12 15:07:40 +0800

    Added devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_11_065815) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_12_065826) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -22,7 +22,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_11_065815) do
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "phone_numbers", charset: "utf8mb4", force: :cascade do |t|
@@ -48,6 +50,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_11_065815) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "students", charset: "utf8mb4", force: :cascade do |t|
@@ -55,6 +59,18 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_11_065815) do
     t.string "student_number"
     t.string "birthdate"
     t.string "timestamps"
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
