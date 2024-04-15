78cc7520484c31401bf9f2c8127cb8ca59d3715a
hannahmeg <hannah_arcega@yahoo.com>
2024-04-15 15:43:53 +0800

    Modified seeds to initialize users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_12_065826) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_15_072148) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -69,6 +69,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_12_065826) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
+    t.string "address", null: false
+    t.string "phone_number", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,21 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+100.times do
+  user = User.create(email: Faker::Internet.email, password: "123456", username: Faker::Internet.unique.user_name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone_in_e164)
+end
+
 100.times do |index|
-  post = Post.create(title: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 0).chop,
-                     content: Faker::Lorem.paragraph)
+  user = User.all.sample  # Get a random user
+  post = user.posts.create(title: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 0).chop,
+                           content: Faker::Lorem.paragraph)
 
   num_categories = rand(1..4)
 
   categories = ['Technology', 'Travel', 'Lifestyle', 'Fashion', 'Food'].sample(num_categories)
 
   categories.each do |category_name|
-    post.categories.create(name: category_name)
+    post.categories.find_or_create_by(name: category_name)
   end
 end
 
