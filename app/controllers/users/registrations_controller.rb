9ed9d9df8a02bba0c3c22083cc31684ff0eee28b
hannahmeg <hannah_arcega@yahoo.com>
2024-04-16 15:53:04 +0800

    removed unnecessary lines

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -45,10 +45,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :address, :phone_number])
   end
 
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :address, :phone_number])
-  end
-
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
