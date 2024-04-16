c7054b40898293bc6b7411fdbc2cb57edb4fc4ce
hannahmeg <hannah_arcega@yahoo.com>
2024-04-16 14:10:00 +0800

    Removed unnecessary lines

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,16 +7,13 @@ class PostsController < ApplicationController
   before_action :validate_post_owner, only: [:edit, :update, :destroy]
 
   def index
-    # render json: params
     @posts = Post.includes(:categories, :user).all
 
-    # Filter by category
     if params[:category].present?
       category_names = params[:category]
       @posts = @posts.joins(:categories).where(categories: { name: category_names })
     end
 
-    # Filter by username
     if params[:username].present?
       @posts = @posts.joins(:user).where("users.username LIKE ?", "%#{params[:username]}%")
     end
