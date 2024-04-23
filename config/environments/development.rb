6d17ceeb24900cd4a885ca23ff54f5dee9bd53da
hannahmeg <hannah_arcega@yahoo.com>
2024-04-23 15:34:53 +0800

    Added config host for ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts <<  "3391-175-176-31-24.ngrok-free.app"
 end
