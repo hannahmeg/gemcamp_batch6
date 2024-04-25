e44ab337e496097ae07ad2bcdf7768c80eb91d4d
hannahmeg <hannah_arcega@yahoo.com>
2024-04-25 15:49:14 +0800

    Added balance column to users

--- db/schema.rb
+++ db/schema.rb
@@ -110,6 +110,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_25_062045) do
     t.string "phone_number", null: false
     t.string "image"
     t.integer "role", default: 0
+    t.decimal "balance", precision: 18, scale: 2, default: "0.0"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
