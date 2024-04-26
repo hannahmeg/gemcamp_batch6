ef9aa9b862f1628cd4fbdc8ff2b1c5550fb04503
hannahmeg <hannah_arcega@yahoo.com>
2024-04-26 14:46:16 +0800

    Added order filter and added condition on methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,17 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:submit, :pay, :fail, :revoke]
+  before_action :authenticate_user!
+  before_action :validate_admin_role
 
   def index
-    @orders = Order.all.includes(:user)
+    @orders = Order.includes(:user)
+
+    @orders = @orders.order(created_at: params[:sort_by] == "asc" ? :asc : :desc)
+    @orders = @orders.page(params[:page]).per(10)
   end
 
   def submit
-    if @order.submit!
+    if @order.may_submit? && @order.submit!
       redirect_to orders_path, notice: 'Order submitted successfully.'
     else
       redirect_to orders_path, alert: 'Failed to submit order.'
@@ -14,7 +19,7 @@ class OrdersController < ApplicationController
   end
 
   def pay
-    if @order.pay!
+    if @order.may_pay? && @order.pay!
       redirect_to orders_path, notice: 'Order paid successfully.'
     else
       redirect_to orders_path, alert: 'Failed to pay order.'
@@ -22,7 +27,7 @@ class OrdersController < ApplicationController
   end
 
   def fail
-    if @order.fail!
+    if @order.may_fail? && @order.fail!
       redirect_to orders_path, notice: 'Order failed.'
     else
       redirect_to orders_path, alert: 'Failed to mark order as failed.'
@@ -30,7 +35,7 @@ class OrdersController < ApplicationController
   end
 
   def revoke
-    if @order.revoke!
+    if @order.may_revoke? && @order.revoke!
       redirect_to orders_path, notice: 'Order revoked.'
     else
       redirect_to orders_path, alert: 'Failed to revoke order.'
@@ -41,5 +46,12 @@ class OrdersController < ApplicationController
 
   def set_order
     @order = Order.find(params[:id])
+  end
+
+  def validate_admin_role
+    unless current_user.role == 'admin'
+      flash[:notice] = 'access unauthorized'
+      redirect_to posts_path
+    end
   end
 end
